fda2da36221f643f1f02cda3258d1319c1949d9f
harsh <harsh.rayzada@gmail.com>
2023-10-27 21:34:16 +0200

    added db service and route handlers

--- main.go
+++ main.go
@@ -1,21 +1,104 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"log"
 	"net/http"
+	"services/DatabaseService"
 )
 
-func getCars() {
-
+type Response struct {
+	message string
+	data    *DatabaseService.Car
 }
 
-func addCar() {
+func getCars() DatabaseService.Car {
+	rows, err := DatabaseService.read("select * from public.\"Cars\"")
+	if err != nil {
+		log.Fatal(err)
+	}
 
+	var cars []DatabaseService.Car
+	for rows.Next() {
+		var car DatabaseService.Car
+		if err := rows.Scan(&car.Make, &car.Model, &car.Color, &car.Power, &car.Year, &car.id); err != nil { //how to read as a single car object instead of individual fields
+			log.Fatal(err)
+		}
+		cars = append(cars, car)
+	}
+
+	return cars
 }
 
-func removeCar() {
+func addCar(r *http.Request) []DatabaseService.Car {
+	decoder := json.NewDecoder(r.Body)
+	var car DatabaseService.Car
+	err := decoder.Decode(&car)
+	if err != nil {
+		fmt.Println("Error while reading request body")
+		log.Fatal(err)
+	}
 
+	var cars []DatabaseService.Car
+	cars, err := DatabaseService.write(car.Make, car.Model, car.Color, car.Power, car.Year)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return cars
 }
 
-func main(r *http.Request, w *http.ResponseWriter) {
+func removeCar(r *http.Request) Response {
+	decoder := json.NewDecoder(r.Body)
+	var car DatabaseService.Car
+	err := decoder.Decode(&car.id)
+	if err != nil {
+		fmt.Println("Error while reading request body")
+		log.Fatal(err)
+	}
 
+	//return json with key message and value as the returned value from db function
+	msg, err := DatabaseService.delete(car.id)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	resp := Response{message: msg}
+	return resp
+}
+
+func routeHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/cars" {
+		http.Error(w, "404 not found", http.StatusNotFound)
+		return
+	}
+
+	switch r.Method {
+	case "GET":
+		cars := getCars()
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(cars)
+	case "POST":
+		cars := addCar(r)
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(cars)
+	case "DELETE":
+		resp := removeCar()
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(resp)
+	default:
+		http.Error(w, "406 Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+}
+
+func main() {
+	http.HandleFunc("/cars", routeHandler)
+
+	if err := http.ListenAndServe(":3000", nil); err != nil {
+		log.Fatal(err)
+	} else {
+		DatabaseService.init()
+	}
 }
---
+++ services/db-service.go
@@ -0,0 +1,74 @@
+package DatabaseService
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	_ "github.com/lib/pq"
+)
+
+type Car struct {
+	Make  *string `json:"make"`
+	Model *string `json:"model"`
+	Color *string `json:"color"`
+	Power *string `json:"power"`
+	Year  *string `json:"year"`
+	id    int8
+}
+
+var db = nil //how to globally store the db object once the connection is made and share it in different functions
+
+func init() {
+	connStr := "postgres://postgres:pgadmpwd160923@localhost/go-postgres?sslmode=disable"
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		fmt.Println("Could not establish connection to database")
+		log.Fatal(err)
+	}
+	err = db.Ping()
+	if err != nil {
+		fmt.Println("Connection to database failed", err)
+	} else {
+		fmt.Println("Connection to database established")
+	}
+}
+
+func read(query string) (*sql.Rows, error) {
+	rows, err := db.Query(query)
+
+	if err != nil {
+		fmt.Println("Error while fetching data")
+		return nil, err
+	}
+
+	return rows, nil
+}
+
+func write(make string, model string, color string, power string, year string) ([]Car, error) { //how to make this accept an object of struct Car instead of individual arguments
+	cars, err := db.QueryRow(`INSERT INTO public."Cars"("Make", "Model", "Color", "Power", "Year") VALUES ($1, $2, $3, $4, $5) RETURNING *`, make, model, color, power, year).Scan(&cars)
+	if err != nil {
+		fmt.Println("Error while writing data")
+		return nil, err
+	}
+
+	return cars, nil
+}
+
+func delete(id int8) (string, error) {
+	err := db.Query(`DELETE * from public."Cars" where id = $1`, id)
+	if err != nil {
+		return "Error while deleting data", err
+	}
+
+	return "Deleted successfully", nil
+}
+
+func closeDB() {
+	err := db.Close()
+	if err != nil {
+		fmt.Println("Connection could not be closed", err)
+	}
+
+	fmt.Println("Connection closed")
+}
